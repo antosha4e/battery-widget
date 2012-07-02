fb3025fa6d636a8d519abcd9e722cc8a4dc49744
antosha.4e@gmail.com <antosha.4e@gmail.com@fbe673ca-ed17-d15f-d7fc-8809d00dcc43>
2012-07-02 12:44:57 +0000

    ...

--- trunk/src/com/antosha/BatteryReceiver.java
+++ trunk/src/com/antosha/BatteryReceiver.java
@@ -5,9 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.PowerManager;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,22 +16,29 @@ import android.widget.Toast;
  * Date: 14.05.12
  */
 public class BatteryReceiver extends BroadcastReceiver {
+    private int i = 0;
+    private double _level = 0;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-//        int[] appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-        int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        double scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        double level = -1;
-        if (rawlevel >= 0 && scale > 0) {
-            level = rawlevel / scale * 100;
-        }
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+//        if (pm.isScreenOn()) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            double scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            double level = -1;
+            if (rawlevel >= 0 && scale > 0) {
+                level = rawlevel / scale * 100;
+            }
 
-//        Toast.makeText(context, "Level is " + level + "/" + scale, Toast.LENGTH_SHORT).show();
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-//        remoteViews.setTextViewText(R.id.percent, "Level is " + level + "/" + scale);
-        remoteViews.setTextViewText(R.id.percent, "" + level);
-//        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-        appWidgetManager.updateAppWidget(new ComponentName(context, BatteryWidget.class), remoteViews);
+        if(_level == level) {
+            return;
+        }
+        _level = level;
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+            remoteViews.setTextViewText(R.id.percent, ++i + " : " + level);
+            appWidgetManager.updateAppWidget(new ComponentName(context, BatteryWidget.class), remoteViews);
+//        }
     }
 }
--- trunk/src/com/antosha/BatteryWidget.java
+++ trunk/src/com/antosha/BatteryWidget.java
@@ -1,6 +1,5 @@
 package com.antosha;
 
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -8,11 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
-import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,15 +20,6 @@ public class BatteryWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         service = new Intent(context, BatteryService.class);
         context.startService(service);
-
-//        Toast.makeText(context, "onUpdate(1)", Toast.LENGTH_LONG).show();
-        //Создаем новый RemoteViews
-//        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-
-//        remoteViews.setTextViewText(R.id.percent, "" + new Random().nextInt(100));
-
-        //обновляем виджет
-//        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
 
     @Override
@@ -46,8 +31,7 @@ public class BatteryWidget extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
-        Intent batteryIntent = context.getApplicationContext().registerReceiver(null,
-                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        Intent batteryIntent = context.getApplicationContext().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         int rawlevel = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         double scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         double level = -1;
@@ -62,42 +46,6 @@ public class BatteryWidget extends AppWidgetProvider {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
 
         remoteViews.setTextViewText(R.id.percent, "Level is " + level);
-//        remoteViews.setTextViewText(R.id.percent, "" + i++);
-
-        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-
-
-
-
-//        Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
-//            public void run() {
-//                ;//wl.release();
-//            }
-//        }, 10, TimeUnit.SECONDS);
-    }
-
-    //    @Override
-    public void onReceive1(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        Toast.makeText(context, "onReceive(1)", Toast.LENGTH_LONG).show();
-
-//        if (UPDATE_WIDGET.equals(intent.getAction())) {
-//            Toast.makeText(context, "FUUUCK", Toast.LENGTH_LONG).show();
-//        }
-
-
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-        int[] appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-
-        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-        Toast.makeText(context, "Level is " + level + "/" + scale, Toast.LENGTH_SHORT).show();
-
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-
-        remoteViews.setTextViewText(R.id.percent, "Level is " + level + "/" + scale);
 
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
