54de0d1aec2d4171cadababb87a4736d54870ad5
antosha.4e@gmail.com <antosha.4e@gmail.com@fbe673ca-ed17-d15f-d7fc-8809d00dcc43>
2012-07-02 14:24:08 +0000

    ...

--- trunk/src/com/antosha/BatteryWidget.java
+++ trunk/src/com/antosha/BatteryWidget.java
@@ -1,12 +1,21 @@
 package com.antosha;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.os.BatteryManager;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,6 +29,34 @@ public class BatteryWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         service = new Intent(context, BatteryService.class);
         context.startService(service);
+
+        //lol(context);
+    }
+
+    private void lol(Context context) {
+        Toast.makeText(context, "is null", Toast.LENGTH_LONG);
+        Resources res = context.getApplicationContext().getResources();
+        if (res == null) {
+            Toast.makeText(context, "res null", Toast.LENGTH_LONG);
+            return;
+        }
+        AssetManager assetManager = res.getAssets();
+        if (assetManager == null) {
+            Toast.makeText(context, "asset null", Toast.LENGTH_LONG);
+            return;
+        }
+        try {
+            InputStream is = assetManager.open("config.properties");
+            if (is != null) {
+                Properties properties = new Properties();
+                properties.load(new BufferedInputStream(is));
+                Toast.makeText(context, properties.getProperty("theme", "lol"), Toast.LENGTH_LONG);
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(context, "is null", Toast.LENGTH_LONG);
     }
 
     @Override
@@ -43,7 +80,11 @@ public class BatteryWidget extends AppWidgetProvider {
 
         int[] appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 
+        Intent pendingIntent = new Intent(context, BatteryActivity.class);
+        PendingIntent pendingintent = PendingIntent.getActivity(context, 0, pendingIntent, 0);
+
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        remoteViews.setOnClickPendingIntent(R.id.main, pendingintent);
 
         remoteViews.setTextViewText(R.id.percent, "Level is " + level);
 
